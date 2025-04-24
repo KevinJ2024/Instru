d3dbec5343f2f762fdd7eae9a211dc854bf31506
Zamara-Osorio <zamaraosorio88@gmail.com>
2025-04-24 14:26:01 -0500

    modificacion de estilo

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,9 +7,9 @@ const {id, title, price, description, category, image, rating} = product;
     <div className="product-card">
         <h1 className='title-card'>{title}</h1>
         <img src={image} alt={title} className="product-image"/>
-        <h3>{price}</h3>
-        <h3>{description}</h3>
-        <h3>{category}</h3>
+        <h3>{price} $</h3>
+        <h3 className='text-card'>{description}</h3>
+        <h3>Category: {category}</h3>
         <div className="rating">
             <h3>Rating: {rating.rate}</h3>
             <h3>Count: {rating.count}</h3>
--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <footer className="footer">
-        <p>Todos los derechos reservados &copy; 2023</p>
+        <p>Todos los derechos reservados &copy; 2025</p>
         <div className='info'>
           <p>Desarrollado por: Kevin Julian Alvarez Ramirez</p>
           <p>Contacto: 1245678</p>
