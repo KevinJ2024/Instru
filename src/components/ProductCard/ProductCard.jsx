2f563e9c98551ca688983d20c2bcdbd82a5aad37
Zamara-Osorio <zamaraosorio88@gmail.com>
2025-04-24 13:23:25 -0500

    avance para la muestra de los productos

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+
+export const ProductCard = ({key, product}) => {
+const URL = 'https://fakestoreapi.com/products'
+
+const {id, title, price, description, category, image, rating} = product;
+
+  return (
+    <div className="product-card" key={key}>
+        <h1>{title}</h1>
+        <img src={image} alt={title} />
+        <h3>{price}</h3>
+        <h3>{description}</h3>
+        <h3>{category}</h3>
+        <div className="rating">
+            <h3>Rating: {rating.rate}</h3>
+            <h3>Count: {rating.count}</h3>
+        </div>
+    </div>
+  )
+}
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,27 @@
+import { ProductCard } from '../../components/ProductCard/ProductCard'
 import './Main.css'
 
 
 export const Main = () => {
+
+  const [products, setProducts] = useState([])
+
+  const handleGetProducts = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setProducts(data.results)
+  }
+
   return (
     <main className="main">
-
+    <button onClick={handleGetProducts}>Traer productos</button>
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {
+          products.map(product => (
+            <ProductCard key={product.id} product = {product} />
+          ))
+        }
+      </section>
     </main>
   )
 }
